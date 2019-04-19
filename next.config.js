9d16bee8591fe5e23b032f6595be2ad3893ba533
kun <kunsam624@icloud.com>
2019-04-19 11:25:49 +0800

    fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const withCSS = require('@zeit/next-css');
 const withLess = require('@zeit/next-sass');
 const withTypescript = require('@zeit/next-typescript');
 
-export default withCSS(withLess(withTypescript({
+module.exports = withCSS(withLess(withTypescript({
   webpack(config, options) {
     return config
   }
